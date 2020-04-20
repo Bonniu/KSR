dbc8eb0ca4b37c68760b4c0f984dbff92d32ab7b
Bonniu <radekg23@o2.pl>
2020-04-20 17:16:06 +0200

    base project2, data with 11 attributes, report with attributes

---
+++ Zadanie2_KSR/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace Zadanie2_KSR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
